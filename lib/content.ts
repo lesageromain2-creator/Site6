c9e36f8a58af7d836d3f20fb8dc79850d798d07d
Positano <lesageromain2@gmail.com>
2026-03-31 15:50:08 +0200

    votre message

--- lib/content.ts
+++ lib/content.ts
@@ -1,8 +1,8 @@
-import address from '../../content/address.json';
-import brands from '../../content/brands.json';
-import imagesMap from '../../content/images-map.json';
-import reviews from '../../content/reviews.json';
-import brand from '../../brand.json';
+import address from '@/content/address.json';
+import brands from '@/content/brands.json';
+import imagesMap from '@/content/images-map.json';
+import reviews from '@/content/reviews.json';
+import brand from '@/brand.json';
 
 export type AddressContent = typeof address;
 export type BrandsContent = typeof brands;
